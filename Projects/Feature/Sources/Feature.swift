23c947d1e6f0452f5d8789c73590919a9f9a0363
uuuunseo <jdbstjoo@gmail.com>
2023-08-20 22:54:14 +0900

    :sparkles: :: Feature
    :: Feature 모듈 구성 완료

---
+++ Projects/Feature/Sources/Feature.swift
@@ -0,0 +1,8 @@
+//
+//  Feature.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 정윤서 on 2023/08/20.
+//
+
+import Foundation
---
+++ Projects/Feature/Tests/FeatureTests.swift
@@ -0,0 +1,8 @@
+//
+//  FeatureTests.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 정윤서 on 2023/08/20.
+//
+
+import Foundation
